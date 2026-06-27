0bf2aef559753dc135622583df0ca44a119c1d2d
tsr749 <tusharraghatate10@gmail.com>
2026-06-27 17:27:06 +0530

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,11 +63,7 @@ export const metadata: Metadata = {
   },
   generator: "v0.app",
   icons: {
-    icon: [
-      { url: "/icon-light-32x32.png", media: "(prefers-color-scheme: light)" },
-      { url: "/icon-dark-32x32.png", media: "(prefers-color-scheme: dark)" },
-      { url: "/icon.svg", type: "image/svg+xml" },
-    ],
+    icon: "/favicon.ico",
     apple: "/apple-icon.png",
   },
 }
