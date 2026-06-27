b0250763c034ec308b7254c08fa268fa80b8a68f
tsr749 <tusharraghatate10@gmail.com>
2026-06-27 15:59:34 +0530

    updated ui

--- lib/site.ts
+++ lib/site.ts
@@ -5,6 +5,7 @@ export const siteConfig = {
   phone2: "+91 814 992 2353",
   phoneHref: "+91 930 708 4168",
   phone2Href: "+91 814 992 2353",
+  whatsapp: "919307084168",
   address: "Plot No. 3, Takli Sim, Near- Metro Piller No. 104, Hingna Road, Nagpur-440036, Maharashtra",
   hours: "Mon – Sat \n 9:30 AM – 7:00 PM IST",
 }
